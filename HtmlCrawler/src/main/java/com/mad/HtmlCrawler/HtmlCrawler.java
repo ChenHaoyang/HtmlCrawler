a01b924cccd314fb1898dcdb5a0b15bba24b86d6
charles <hust.chenhaoyang@gmail.com>
2016-06-22 16:47:24 +0900

    getHtml->HBase

--- HtmlCrawler/src/main/java/com/mad/HtmlCrawler/HtmlCrawler.java
+++ HtmlCrawler/src/main/java/com/mad/HtmlCrawler/HtmlCrawler.java
@@ -56,9 +56,9 @@ public class HtmlCrawler
 	private CloseableHttpClient m_httpClient;
 	private RequestConfig m_requestConfig;
 	private UniversalDetector m_uDetector;
-	public Configuration m_dfsConfig, m_hbaseConfig;
+	public Configuration /*m_dfsConfig,*/ m_hbaseConfig;
 	private String m_accessUser;
-	public FileSystem m_dfs;
+//	public FileSystem m_dfs;
 	private String m_hdfsPath;
 	
 	public HtmlCrawler(){
@@ -72,11 +72,11 @@ public class HtmlCrawler
 				.build();
 		m_uDetector = new UniversalDetector(null);
 		HttpURLConnection.setFollowRedirects(true);
-		m_dfsConfig = new Configuration();
+//		m_dfsConfig = new Configuration();
 		m_accessUser = "spark";
 		//m_dfsConfig.set("fs.default.name", "hdfs://172.25.198.11:8020/");
-		m_dfsConfig.addResource(new Path("/usr/local/hadoop-2.5.0-cdh5.3.9/etc/hadoop/core-site.xml"));
-		m_dfsConfig.addResource(new Path("/usr/local/hadoop-2.5.0-cdh5.3.9/etc/hadoop/hdfs-site.xml"));
+//		m_dfsConfig.addResource(new Path("/usr/local/hadoop-2.5.0-cdh5.3.9/etc/hadoop/core-site.xml"));
+//		m_dfsConfig.addResource(new Path("/usr/local/hadoop-2.5.0-cdh5.3.9/etc/hadoop/hdfs-site.xml"));
 		
 		m_hdfsPath = "/user/" + m_accessUser + "/html/";
 		//UserGroupInformation ugi = UserGroupInformation.createRemoteUser("hdfs");
@@ -92,22 +92,22 @@ public class HtmlCrawler
 		
 		//System.out.print(m_dfsConfig.get("fs.defaultFS"));
 		//System.out.print(m_dfsConfig.get("hadoop.job.ugi"));
-		try {
-			m_dfs = FileSystem.get(FileSystem.getDefaultUri(m_dfsConfig),m_dfsConfig,m_accessUser);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		try {
+//			m_dfs = FileSystem.get(FileSystem.getDefaultUri(m_dfsConfig),m_dfsConfig,m_accessUser);
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 	}
 	
-	protected void finalize(){
-		try {
-			m_dfs.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+//	protected void finalize(){
+//		try {
+//			m_dfs.close();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
 	
 	public String getHtml(String url){	
 		String html=null, encoding;
@@ -170,7 +170,7 @@ public class HtmlCrawler
 			response.close();
 		}
 		catch(Exception e){
-			System.out.println(e.toString());
+			//System.out.println(e.toString());
 		}
 		finally{
 			m_uDetector.reset();
@@ -201,86 +201,86 @@ public class HtmlCrawler
 		return true;
 	}
 	
-	public boolean writeToHDFS(String contents, String name){
-		return writeToHDFS(contents, name, false);
-	}
+//	public boolean writeToHDFS(String contents, String name){
+//		return writeToHDFS(contents, name, false);
+//	}
 	
-	public boolean writeToHDFS(String contents, String name, boolean append){	
-		FSDataOutputStream os;
-		Path p = new Path(m_hdfsPath + name + ".txt");
-    	try {
-    		if(!append){
-//	    		if(m_dfs.exists(p)){
-//	    			return true;
-//	    			//m_dfs.delete(p, false);
-//	    		}
-				os = m_dfs.create(p);
-    		}
-    		else{
-    			if(!m_dfs.exists(p)){
-    				os = m_dfs.create(p);
-    			}
-    			else{
-    				os = m_dfs.append(p);
-    			}
-    		}
-    		os.write(contents.getBytes("UTF-8"));
-			os.close();
-    			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			
-			//os.close();
-			return false;
-		}
-		return true;
-	}
+//	public boolean writeToHDFS(String contents, String name, boolean append){	
+//		FSDataOutputStream os;
+//		Path p = new Path(m_hdfsPath + name + ".txt");
+//    	try {
+//    		if(!append){
+////	    		if(m_dfs.exists(p)){
+////	    			return true;
+////	    			//m_dfs.delete(p, false);
+////	    		}
+//				os = m_dfs.create(p);
+//    		}
+//    		else{
+//    			if(!m_dfs.exists(p)){
+//    				os = m_dfs.create(p);
+//    			}
+//    			else{
+//    				os = m_dfs.append(p);
+//    			}
+//    		}
+//    		os.write(contents.getBytes("UTF-8"));
+//			os.close();
+//    			
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//			
+//			//os.close();
+//			return false;
+//		}
+//		return true;
+//	}
 	
-	public String readFromHdfs(String name){
-		Path p = new Path(m_hdfsPath + name + ".txt");
-		String html="", tmp;
-		try{
-			FSDataInputStream in = m_dfs.open(p);
-			BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-			while((tmp=br.readLine()) != null){
-				html += (tmp + "\n");
-			}
-			br.close();
-			in.close();
-		}
-		catch(Exception e){
-			e.printStackTrace();
-		}
-		//System.out.print(html);
-		return html.trim();
-	}
+//	public String readFromHdfs(String name){
+//		Path p = new Path(m_hdfsPath + name + ".txt");
+//		String html="", tmp;
+//		try{
+//			FSDataInputStream in = m_dfs.open(p);
+//			BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+//			while((tmp=br.readLine()) != null){
+//				html += (tmp + "\n");
+//			}
+//			br.close();
+//			in.close();
+//		}
+//		catch(Exception e){
+//			e.printStackTrace();
+//		}
+//		//System.out.print(html);
+//		return html.trim();
+//	}
 	
-	public boolean writeSequenceFile(String key, String value, String name){
-		LongWritable key_ = new LongWritable();
-		Text val_ = new Text();
-		Path path = new Path(m_hdfsPath + name + ".dat");
-		SequenceFile.Writer writer = null;
-		try{
-			writer = SequenceFile.createWriter(m_dfsConfig,
-					Writer.file(path),
-					Writer.keyClass(key_.getClass()),
-					Writer.valueClass(val_.getClass()),
-					Writer.compression(SequenceFile.CompressionType.BLOCK, new DefaultCodec()),
-					Writer.metadata(new Metadata()));
-			
-			val_.set("1	test");
-			key_.set(Long.parseLong(key));
-			writer.append(key_, val_);
-			writer.close();
-		}
-		catch(Exception e){
-			e.printStackTrace();
-		}
-		
-		
-		return true;
-	}
+//	public boolean writeSequenceFile(String key, String value, String name){
+//		LongWritable key_ = new LongWritable();
+//		Text val_ = new Text();
+//		Path path = new Path(m_hdfsPath + name + ".dat");
+//		SequenceFile.Writer writer = null;
+//		try{
+//			writer = SequenceFile.createWriter(m_dfsConfig,
+//					Writer.file(path),
+//					Writer.keyClass(key_.getClass()),
+//					Writer.valueClass(val_.getClass()),
+//					Writer.compression(SequenceFile.CompressionType.BLOCK, new DefaultCodec()),
+//					Writer.metadata(new Metadata()));
+//			
+//			val_.set("1	test");
+//			key_.set(Long.parseLong(key));
+//			writer.append(key_, val_);
+//			writer.close();
+//		}
+//		catch(Exception e){
+//			e.printStackTrace();
+//		}
+//		
+//		
+//		return true;
+//	}
 	
 	public boolean writeToHBase(BufferedMutator table, String row_key, String family, String qualifier, String value) throws IOException{
 		Put p = new Put(Bytes.toBytes(row_key));
@@ -299,18 +299,18 @@ public class HtmlCrawler
     	String line_str, err_msg;
     	String[] tokens;
     	Connection conn=null;
-    	//Table table = null;
+    	Table table = null;
     	BufferedMutator mutator=null;
+    	long skip_count=0, insert_count=0, error_count=0;
+    	long skip_time=0, insert_time=0, error_time=0;
     	
     	HtmlCrawler crawler = new HtmlCrawler();
     	//crawler.writeToLocal("test","abc",true);
-
-    	
     	try{
     		//HTable table = new HTable(crawler.m_hbaseConfig, "test");
     		conn = ConnectionFactory.createConnection(crawler.m_hbaseConfig);
     		//System.out.println(crawler.m_hbaseConfig.get("hbase.client.write.buffer"));
-    		//table = conn.getTable(TableName.valueOf("test"));
+    		table = conn.getTable(TableName.valueOf("url_info"));
     		mutator = conn.getBufferedMutator(TableName.valueOf("url_info"));
 //        	Get g = new Get(Bytes.toBytes("1"));
 //        	Result r = table.get(g);
@@ -320,7 +320,7 @@ public class HtmlCrawler
 //        	table.put(p);
     		br = new BufferedReader(new InputStreamReader(new FileInputStream("/home/charles/Data/input/"+args[0])));
     		line_str = br.readLine();
-    		
+    		//long time = System.currentTimeMillis();
     		while(line_str != null && !"".equals(line_str)){
     			long time = System.currentTimeMillis();
     			tokens = line_str.split(",");
@@ -329,35 +329,48 @@ public class HtmlCrawler
     				System.out.print(err_msg);
     				continue;
     			}
-    			tokens[0] = "717855136";
-    			tokens[1] = "http://news.biglobe.ne.jp/topics/entertainment/0309/00197.html";
-    			System.out.print(tokens[0] + " : ");
-    			Path tmp_path = new Path(crawler.m_hdfsPath + tokens[0] + ".txt");
-    			if(crawler.m_dfs.exists(tmp_path)){
+    			//Path tmp_path = new Path(crawler.m_hdfsPath + tokens[0] + ".txt");
+				Get g = new Get(Bytes.toBytes(tokens[0]));
+				Result r = table.get(g);
+				System.out.print(tokens[0] + ": ");
+    			//if(crawler.m_dfs.exists(tmp_path)){
+				if(r.isEmpty()){
+	    			String html = crawler.getHtml(tokens[1]);
+	    			if(html != null && !"".equals(html)){
+	    				crawler.writeToHBase(mutator, tokens[0], "raw_html", null, html);
+	    				System.out.print("INSERT\n");
+	    				insert_count++;
+	    				insert_time += (System.currentTimeMillis()-time);
+	    			}else{
+	    				err_msg = line_str + "\n"; 
+	    				crawler.writeToLocal(err_msg, "error_" + args[1], true);
+	    				System.out.print("Local\n");
+	    				error_count++;
+	    				error_time += (System.currentTimeMillis()-time);
+	    			}
     				//crawler.writeSequenceFile(tokens[0], crawler.readFromHdfs(tokens[0]), "seq_"+args[1]);
-    				crawler.writeToHBase(mutator, tokens[0], "raw_html", null, crawler.readFromHdfs(tokens[0]));
+    				//crawler.writeToHBase(mutator, tokens[0], "raw_html", null, crawler.readFromHdfs(tokens[0]));
     				//mutator.flush();
-    				System.out.print("SKIP\n");
-    				line_str = br.readLine();
-    				continue;
-    			}
-    			String html = crawler.getHtml(tokens[1]);
-    			//System.out.print("Html, ");
-    			if(html != null && !"".equals(html)){
-    				//crawler.writeToHDFS(html, tokens[0]);
-    				//crawler.writeSequenceFile(tokens[0], html, "seq_"+args[1]);
-    				crawler.writeToHBase(mutator, tokens[0], "raw_html", null, html);
-    				System.out.print("HDFS\n");
-    			}else{
-    				err_msg = line_str + "\n"; 
-    				crawler.writeToLocal(err_msg, "error_" + args[1], true);
-    				System.out.print("Local\n");
-    			}
+				}else{
+					System.out.print("SKIP\n");
+					skip_count++;
+					skip_time += (System.currentTimeMillis()-time);
+				}
+				//crawler.m_dfs.delete(tmp_path, false);
+				//line_str = br.readLine();
+				//continue;
+    			//}
     			line_str = br.readLine();
-    			System.out.println("Run Time: " + (System.currentTimeMillis()-time) + "ms");
     		}
+   			//System.out.println("Run Time: " + (System.currentTimeMillis()-time) + "ms");
+   			System.out.println("INSERTED: " + insert_count);
+   			System.out.println("INSERT_TIME: " + insert_time);
+   			System.out.println("SKIPPED: " + skip_count);
+   			System.out.println("SKIP_TIME: " + skip_time);
+   			System.out.println("ERROR: " + error_count);
+   			System.out.println("ERROR_TIME: " + error_time);
     		br.close();
-    		//table.close();
+    		table.close();
     		mutator.close();
     		conn.close();
     		
